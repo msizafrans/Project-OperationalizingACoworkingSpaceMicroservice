51dd1d4cd23f42e1b22902eaa2b5742a60eb0eb2
Frans <135562158+msizafrans@users.noreply.github.com>
2023-11-22 16:45:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ from datetime import datetime, timedelta
 from flask import jsonify, request
 from sqlalchemy import and_, text
 from random import randint
-from models import Token
+
 from config import app, db
 
+
 port_number = int(os.environ.get("APP_PORT", 5153))
+
 
 @app.route("/health_check")
 def health_check():
@@ -30,11 +32,11 @@ def readiness_check():
 def get_daily_visits():
     with app.app_context():
         result = db.session.execute(text("""
-            SELECT Date(created_at) AS date,
-                Count(*) AS visits
-            FROM tokens
-            WHERE used_at IS NOT NULL
-            GROUP BY Date(created_at)
+        SELECT Date(created_at) AS date,
+            Count(*)         AS visits
+        FROM   tokens
+        WHERE  used_at IS NOT NULL
+        GROUP  BY Date(created_at)
         """))
 
         response = {}
@@ -48,7 +50,7 @@ def get_daily_visits():
 
 @app.route("/api/reports/daily_usage", methods=["GET"])
 def daily_visits():
-    return get_daily_visits()
+    return jsonify(get_daily_visits)
 
 
 @app.route("/api/reports/user_visits", methods=["GET"])
@@ -71,7 +73,7 @@ def all_user_visits():
             "visits": row[1],
             "joined_at": str(row[2])
         }
-
+    
     return jsonify(response)
 
 
